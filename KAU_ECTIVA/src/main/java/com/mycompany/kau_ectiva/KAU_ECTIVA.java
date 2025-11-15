91373d4bd18c53cd45eff32dd79947dfbcbec126
somya <somya@DESKTOP-F5ETUJH>
2025-11-15 14:22:19 +0300

    Initial commit - test connection

---
+++ KAU_ECTIVA/src/main/java/com/mycompany/kau_ectiva/KAU_ECTIVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kau_ectiva;
+
+/**
+ *
+ * @author somya
+ */
+public class KAU_ECTIVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
