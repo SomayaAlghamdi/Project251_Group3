2bd8ae2174f5d06960f1adfeea60176ba313460f
somya <somya@DESKTOP-F5ETUJH>
2025-11-15 17:03:06 +0300

    Add packages and empty classes

---
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/app/App.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group3.kau_ectiva.app;
+
+/**
+ *
+ * @author somya
+ */
+public class App {
+     public static void main(String[] args) {
+          }
+}
