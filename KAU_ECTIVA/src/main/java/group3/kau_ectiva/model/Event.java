83c32ed893a0642d4509568f58c6418ce0dab1fa
JanaB <JanaB@JanaBawaked>
2025-11-16 23:47:03 +0300

    Edit add Event model

--- KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Event.java
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Event.java
@@ -29,5 +29,69 @@ public class Event {
         this.capacity = capacity;
         this.availableSeats = availableSeats;
     }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
     
 }
