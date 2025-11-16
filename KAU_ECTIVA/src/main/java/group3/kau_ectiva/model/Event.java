232bd3522dcbf25d06670e83d8d82db1681d36cf
JanaB <JanaB@JanaBawaked>
2025-11-16 20:13:02 +0300

    Add Event model

--- KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Event.java
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Event.java
@@ -9,5 +9,25 @@ package group3.kau_ectiva.model;
  * @author somya
  */
 public class Event {
+    private String eventId;
+    private String name;
+    private String description;
+    private String date;
+    private String time;
+    private String location;
+    private int capacity;
+    private int availableSeats;
+    
+    public Event(String eventId, String name, String description,String date, String time, 
+    String location, int capacity, int availableSeats) {
+        this.eventId = eventId;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.capacity = capacity;
+        this.availableSeats = availableSeats;
+    }
     
 }
