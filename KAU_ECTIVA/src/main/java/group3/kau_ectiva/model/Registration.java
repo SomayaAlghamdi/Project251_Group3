c14edfdafcc9a2d948eccee4fa71bccefb754495
Wjood <Wjood@WJOOD-PC>
2025-11-16 23:23:11 +0300

    Implemened Register

--- KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Registration.java
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Registration.java
@@ -9,5 +9,39 @@ package group3.kau_ectiva.model;
  * @author somya
  */
 public class Registration {
+    private String studentId;
+    private String eventId;
+    private String registrationDate;
+    
+    public Registration(String studentId, String eventId, String registrationDate) {
+        this.studentId = studentId;
+        this.eventId = eventId;
+        this.registrationDate = registrationDate;
+    }
+    public String getStudentId() { 
+        return studentId; 
+    }
+    
+    public String getEventId() {
+        return eventId; 
+    }
+    public String getRegistrationDate() { 
+        return registrationDate; 
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+    
+    
+    
     
 }
