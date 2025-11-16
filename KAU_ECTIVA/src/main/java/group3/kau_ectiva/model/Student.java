6679318c7575232e66333e13c4c2e122f58e7b73
somya <somya@DESKTOP-F5ETUJH>
2025-11-16 19:36:07 +0300

    Add Student model class (fields + constructor + getters)

--- KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Student.java
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/model/Student.java
@@ -3,11 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package group3.kau_ectiva.model;
-
 /**
  *
  * @author somya
  */
 public class Student {
+    private String studentId;
+    private String SName;
+    private String email;
+
+    public Student(String studentId, String name, String email) {
+        this.studentId = studentId;
+        this.SName = name;
+        this.email = email;
+    }
+
+    public void setStudentId(String studentId){ 
+        this.studentId = studentId;
+    }
+    public void setName(String name){ 
+        this.SName = name; 
+    }
+    public void setEmail(String email){ 
+        this.email = email;
+    }
     
+    public String getStudentId(){ 
+        return studentId; 
+    }
+    public String getName(){ 
+        return SName; 
+    }
+    public String getEmail(){ 
+        return email; 
+    }
+
 }
