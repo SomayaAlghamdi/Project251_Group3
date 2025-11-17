152906183dccdce345b7e07725c636da40455a5e
JanaB <JanaB@JanaBawaked>
2025-11-17 21:49:35 +0300

    add Event service

--- KAU_ECTIVA/src/main/java/group3/kau_ectiva/service/EventService.java
+++ KAU_ECTIVA/src/main/java/group3/kau_ectiva/service/EventService.java
@@ -8,6 +8,36 @@ package group3.kau_ectiva.service;
  *
  * @author somya
  */
+import group3.kau_ectiva.model.Event;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventService {
+    private List<Event> events = new ArrayList<>();
+
+    public EventService() {
+
+    }
+
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+    
+    public Event findById(String eventId) {
+        for (Event e : events) {
+            if (e.getEventId().equals(eventId)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public List<Event> getAllEvents() {
+        return events;
+    }
+    
     
 }
+
+
+
