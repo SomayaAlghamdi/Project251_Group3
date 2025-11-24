c995a4ed1683b9e4f99121a2731153c3692ef055
somya <somya@DESKTOP-F5ETUJH>
2025-11-24 18:14:31 +0300

    Implement Add Event unit test (testAddEvent_successfullyAdded)

--- KAU_ECTIVA/src/test/java/KAU_ECTIVA_Test.java
+++ KAU_ECTIVA/src/test/java/KAU_ECTIVA_Test.java
@@ -25,5 +25,56 @@ public class KAU_ECTIVA_Test {
     private RegistrationService registrationService;
     private StudentService studentService;
     private ReportService reportService;
+    
+    @Before
+    public void setUp() {
+        eventService = new EventService();
+        studentService = new StudentService();
+        registrationService = new RegistrationService(eventService, studentService);
+        reportService = new ReportService(eventService, registrationService, studentService);
+
+        // Add static test data
+        Event e1 = new Event(
+        "E1",
+        "Scientific Societies Innovation Challenge",
+        "Individual competition for KAU students to present impactful ideas",
+        "2025-11-11",
+        "10:00",
+        "King Faisal Conference Center",
+        3,      // capacity
+        3       // available seats
+        );
+
+        Event e2 = new Event(
+        "E2",
+        "Athar Hackathon",
+        "Innovation & entrepreneurship hackathon for university students",
+        "2025-10-21",
+        "09:00",
+        "KAU Innovation & Entrepreneurship Center",
+        2,      // capacity
+        2       // available seats
+        );
+        eventService.addEvent(e1);
+        eventService.addEvent(e2);
+
+        studentService.addStudent(new Student("S1", "Raghad", "Raghad@stu.kau.edu.sa"));
+        studentService.addStudent(new Student("S2", "Jana", "jana@stu.kau.edu.sa"));
+        studentService.addStudent(new Student("S3", "Wjood", "wjood@stu.kau.edu.sa"));
+    }
+    // -------------------------------------------------------------
+    // 1) Unit Test — Add Event
+    // -------------------------------------------------------------
+    @Test
+    public void testAddEvent_successfullyAdded() {
+        Event newEvent = new Event("E3", "Innovation Hackathon", "University-wide competition for creative ideas",
+                "2025-11-30", "09:00", "KAU Conference Center", 5, 5);
+        eventService.addEvent(newEvent);
+        Event result = eventService.findById("E3");
+
+        assertNotNull("Event should be added", result);//Is the event happening now?
+        assertEquals("Event name should match", "Innovation Hackathon", result.getName());//Is the data correct?
+        assertEquals("Available seats should equal capacity", 5, result.getAvailableSeats());//Does the capacity equal the number of available seats?
+    }
 
 }
