d749280cb6479de6cc563eeacae97403379cbead
somya <somya@DESKTOP-F5ETUJH>
2025-11-24 15:50:08 +0300

    Initial Unit Test Setup – Created KAU_ECTIVA_Test class

---
+++ KAU_ECTIVA/src/test/java/KAU_ECTIVA_Test.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author somya
+ */
+import group3.kau_ectiva.model.Event;
+import group3.kau_ectiva.model.Registration;
+import group3.kau_ectiva.model.Student;
+import group3.kau_ectiva.service.EventService;
+import group3.kau_ectiva.service.RegistrationService;
+import group3.kau_ectiva.service.ReportService;
+import group3.kau_ectiva.service.StudentService;
+
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KAU_ECTIVA_Test {
+    private EventService eventService;
+    private RegistrationService registrationService;
+    private StudentService studentService;
+    private ReportService reportService;
+
+}
